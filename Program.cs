b0f261a9f9ae5ce1916a0801660ba91554e9f575
Carlos_Pena <Carlos_Pena@EPCOBOGW1865>
2024-04-25 19:54:16 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ScaffoldingWorkshop;
+
+using var db = new ChinookContext();
+
+var albums = db.Albums.ToList();
+//var albumsPlusArtist = db.Albums.Include(x => x.Artist);
+    
+//  var result = albumsPlusArtist.ToList();
+
+
+
+Console.WriteLine("Hello, World!");
+
+//Data Source=localhost;Initial Catalog=chinook;Integrated Security=True;Trust Server Certificate=True
---
+++ Repository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScaffoldingWorkshop
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly ChinookContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public Repository(ChinookContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
